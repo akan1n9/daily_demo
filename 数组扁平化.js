1ad0c70084bb2629ab706717e4437df13c8c6be6
akan1n9 <1052756163@qq.com>
2023-05-30 19:18:14 +0800

    Update 数组扁平化.js

--- 数组扁平化.js
+++ 数组扁平化.js
@@ -1,1 +1,11 @@
+// 利用concat拼接数组，实现一层扁平；
 let arr = [1, 2, 3, [4, 5], [7, [8, 9]], 10];
+function flat1(arr) {
+    let res = [];
+    arr.forEach(item => {
+        res = res.concat(item)
+    });
+    return res;
+}
+
+console.log(flat1(arr));
