5c6d71f256c223c92ca91f28a2f223d07ccb7052
akan1n9 <1052756163@qq.com>
2023-05-30 20:11:49 +0800

    5/30

--- 数组扁平化.js
+++ 数组扁平化.js
@@ -1,5 +1,11 @@
+const arr = [1, 2, 3, [4, 5], [7, [8, 9]], 10];
+
+
+//利用ES6数组的flat方法。 
+let arr1 = arr.flat(Infinity)
+console.log(arr1);
+
 // 利用concat拼接数组，实现一层扁平；
-let arr = [1, 2, 3, [4, 5], [7, [8, 9]], 10];
 function flat1(arr) {
     let res = [];
     arr.forEach(item => {
@@ -7,5 +13,4 @@ function flat1(arr) {
     });
     return res;
 }
-
 console.log(flat1(arr));
