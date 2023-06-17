4f3a23775d88fd6e99c79771f81933b02d1a35bf
akan1n9 <1052756163@qq.com>
2023-06-18 01:33:51 +0800

    布局

---
+++ js/write_promise.js
@@ -0,0 +1,53 @@
+
+const PENDING = 'pending';
+const FULFILLED = 'fulfilled';
+const REJECTED = 'rejected';
+
+
+
+
+
+class MyPromise {
+    constructor(executor) {
+        executor(this.reslove, this.reject)
+    }
+
+    status = PENDING;
+    value = null;
+    reason = null;
+    onFulfilledCallbacks = [];
+    onRejectedCallbacks = [];
+
+    reslove = (value) => {
+        if (this.status === PENDING) {
+            this.status = FULFILLED;
+            this.value = value;
+            while (this.onFulfilledCallbacks.length) {
+                this.onFulfilledCallbacks.shift()(value)
+            }
+        }
+    };
+    reject = (reason) => {
+        if (this.status === PENDING) {
+            this.status = REJECTED;
+            this.reason = reason;
+            while (this.onRejectedCallbacks.length) {
+                this.onRejectedCallbacks.shift()(reason)
+            }
+        }
+    };
+    then(onFulfilled, onRejected) {
+        if (this.status === FULFILLED) {
+            onFulfilled(this.value)
+        } else if (this.status === REJECTED) {
+            onRejected(this.reason)
+        } else if (this.status === PENDING) {
+            this.onFulfilledCallbacks.push(onFulfilled);
+            this.onRejectedCallbacks.push(onRejected);
+        }
+    }
+}
+
+module.exports = MyPromise;
+
+
